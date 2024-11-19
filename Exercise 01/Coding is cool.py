71aad08c78a1592d1c09896ed9a643421d723aa9
MarahMaher <marahmaher2021@gmail.com>
2024-11-19 16:52:47 +0400

    Coding is cool

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,9 @@
+# Fill in the blanks below
+
+word1 = "coding " # we named the firts Variable with coding .
+word2 = "is "     # we named the second Variable with is .
+word3 = "cool "   # we named third Variable with cool .
+
+# Use string to combine the variables and print the statement 
+
+print(word1 +word2 +word3)
