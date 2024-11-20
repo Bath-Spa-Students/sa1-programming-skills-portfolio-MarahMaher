e7d3b6ac2c69b51cf1040ec4426ed5aafc325838
MarahMaher <marahmaher2021@gmail.com>
2024-11-20 17:16:39 +0400

    Biography2.py

--- Exercise 03/Biography2.py
+++ Exercise 03/Biography2.py
@@ -1,9 +1,12 @@
 dictionary={}
+# give commands to the code .
 
 first_name = input ("kindly enter your first name:")
 second_name = input ("Kindly enter your second name:")
 dictionary['name'] = first_name+" " + second_name 
 dictionary['hometown']= input ("kindly enter your hometown: ")
+
+# if your age is digit it will print out if not your should write your digit age .
 
 while True :
     age_input=input("kindly enter your age:")
@@ -12,4 +15,6 @@ while True :
         print (f"Name: {dictionary['name']} \nHometown: {dictionary['hometown']} \nAge: {dictionary['age']}")
         break
     else:
+        print("invalid input. kindly enter a numeric value for age.")
+
         print("invalid input. kindly enter a numeric value for age.")
