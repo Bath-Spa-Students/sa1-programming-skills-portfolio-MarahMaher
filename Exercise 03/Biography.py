9259308f2791831ee56f626b5046a62fc0f9d40d
MarahMaher <marahmaher2021@gmail.com>
2024-11-20 12:17:04 +0400

    Update Biography.py

--- Exercise 03/Biography.py
+++ Exercise 03/Biography.py
@@ -1,7 +1,7 @@
-# define a dictionary to store the information
-
+#Store the information (name , hometown , and age )
 dictionary = {'name': 'marah' ,
               'hometown': 'rak' ,
               'age' : '22'}
-
+#Use variables .
+#Print the values on separate lines .
 print (f"Name: {dictionary['name']} \nHometown: {dictionary['hometown']} \nAge: {dictionary['age']}")
