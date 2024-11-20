a91faf0859300dfec6487fd9b08c88142f768d9c
MarahMaher <marahmaher2021@gmail.com>
2024-11-20 16:54:20 +0400

    Primitive Quiz.py

---
+++ Exercise 04/Primitive Quiz.py
@@ -0,0 +1,80 @@
+#Ask the user What is the capital of European countries
+answer = input("What is the capital of France?").strip()
+
+#Check if the answer is correct
+if answer.lower() == "paris":
+    print("Thats Correct")
+else:
+    print("Thats worng , The correct answer is Paris.")
+
+
+answer = input("What is the capital of Belarus ?").strip()
+
+if answer.lower() == "minsk":
+    print("Thats Correct")
+else:
+    print("Thats worng , the correct answer is Minsk")
+
+
+answer = input("What is the capital of Bulgaria ?").strip()
+
+if answer.lower() == "sofia":
+    print("Thats Correct")
+else:
+    print("Thats worng , the correct answer is Sofia")
+
+
+answer = input("What is the capital of Italy?").strip()
+
+if answer.lower() == "rome":
+    print("Thats Correct")
+else:
+    print("Thats worng , the correct answer is Rome")
+
+
+answer = input("What is the capital of Ukraine ?").strip()
+
+if answer.lower() == "kyiv":
+    print("Thats Correct")
+else:
+    print("Thats worng , the correct answer is Kyiv")
+
+
+answer = input("What is the capital of United Kingdom ?").strip()
+
+if answer.lower() == "london":
+    print("Thats Correct")
+else:
+    print("Thats worng , the correct answer is London")
+
+
+answer = input("What is the capital of Spain ?").strip()
+
+if answer.lower() == "madrid":
+    print("Thats Correct")
+else:
+    print("Thats worng , the correct answer is Madrid")
+
+
+answer = input("What is the capital of Sweden ?").strip()
+
+if answer.lower() == "stockholm":
+    print("Thats Correct")
+else:
+    print("Thats worng , the correct answer is Stockholm")
+
+
+answer = input("What is the capital of Romaina ?").strip()
+
+if answer.lower() == "bucharest":
+    print("Thats Correct")
+else:
+    print("Thats worng , the correct answer is Bucharest")
+
+
+answer = input("What is the capital of Portugal ?").strip()
+
+if answer.lower() == "lisbon":
+    print("Thats Correct")
+else:
+    print("Thats worng , the correct answer is Lisbon")
