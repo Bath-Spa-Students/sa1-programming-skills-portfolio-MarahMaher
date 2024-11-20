585a17ea678b5bd915954222d4153a9140db3a81
MarahMaher <marahmaher2021@gmail.com>
2024-11-20 11:50:46 +0400

    Biography.py

---
+++ Exercise 03/Biography.py
@@ -0,0 +1,7 @@
+# define a dictionary to store the information
+
+dictionary = {'name': 'marah' ,
+              'hometown': 'rak' ,
+              'age' : '22'}
+
+print (f"Name: {dictionary['name']} \nHometown: {dictionary['hometown']} \nAge: {dictionary['age']}")
