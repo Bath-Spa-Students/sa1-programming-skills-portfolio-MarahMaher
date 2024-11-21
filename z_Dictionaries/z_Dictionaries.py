be1e8c405f39787777b78094bcc7c3bc539c417f
MarahMaher <marahmaher2021@gmail.com>
2024-11-22 03:16:29 +0400

    z_Dictionaries.py

---
+++ z_Dictionaries/z_Dictionaries.py
@@ -0,0 +1,12 @@
+# Create a dictionary to store information about a student
+student = {
+    "first_name": "Marah",
+    "last_name": "mokbel",
+    "age": 22,
+    "favorite game": "call_of_duty"
+}
+# Print each piece of information stored 
+print("First Name:", student["first_name"])
+print("Last Name:", student["last_name"])
+print("Age:", student["age"])
+print("favorite game:", student["favorite game"])
