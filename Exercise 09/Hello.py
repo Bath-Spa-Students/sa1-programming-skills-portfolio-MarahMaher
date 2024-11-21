c06cda14276901bc6a95abb608596bf58a9850dd
MarahMaher <marahmaher2021@gmail.com>
2024-11-21 11:08:57 +0400

    Hello.py

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,9 @@
+# in this code we will fill the blanks for the result 
+def hello():
+    print("Hello")  # fill the blank
+
+def main():
+    hello()  # fill the blank 
+
+if __name__ == "__main__":
+    main()
