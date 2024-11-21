6c9fa10ad1199107dcaecf8f837f65d3cc09d34e
MarahMaher <marahmaher2021@gmail.com>
2024-11-22 03:11:07 +0400

    z_Functions-Favorite Book.py

---
+++ z_Functions -Favorite Book/z_Functions -Favorite Book.py
@@ -0,0 +1,6 @@
+# Define the function favorite_book
+def favorite_book(name):
+    print(f"My favorite books is {name}.")
+
+# Call the function with a book title
+favorite_book("The Vampire diaries")
