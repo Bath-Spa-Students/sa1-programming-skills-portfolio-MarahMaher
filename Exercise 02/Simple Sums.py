b44a68cb0508af5e307c503fde1e45362008a054
MarahMaher <marahmaher2021@gmail.com>
2024-11-22 03:21:02 +0400

    Update Simple Sums.py

--- Exercise 02/Simple Sums.py
+++ Exercise 02/Simple Sums.py
@@ -1,4 +1,6 @@
-Amount1= 8
-Amount2= 10
+# Simple Sums
+
+Amount1= 8 # initializes a variable Amount1 with the value 8.
+Amount2= 10 # initializes a variable Amount2 with the value 10.
 sum_of_Amount = Amount1 + Amount2 
 print(sum_of_Amount)
