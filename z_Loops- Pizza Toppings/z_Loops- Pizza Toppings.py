5f2240d074b68e23f5b8bb34a09329bee0383b6d
MarahMaher <marahmaher2021@gmail.com>
2024-11-22 19:46:57 +0400

    Delete z_Loops- Pizza Toppings.py

--- z_Loops- Pizza Toppings/z_Loops- Pizza Toppings.py
+++
@@ -1,9 +0,0 @@
-# Loop to prompt the user for pizza toppings
-while True:
-    enter = input("Please enter your flavor (or type 'exit'): ").strip().lower()
-    
-    if enter == 'exit':
-        print("Thank you! We'll start preparing your pizza.")
-        break
-    else:
-        print(f"I'll add {enter} to your pizza.")
